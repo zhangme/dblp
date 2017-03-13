3e2cc6ff960de196fd9246d40661624b92c0d252
Meng Zhang <mengzhang@mengs-mbp.gateway.pace.com>
2017-03-12 20:49:20 -0400

    basic stats

---
+++ getStats.py
@@ -0,0 +1,16 @@
+import networkx as nx
+
+dblp = nx.read_pajek("dblp.net")
+
+print "nodes: ", dblp.number_of_nodes() #1896883
+print "edges: ", dblp.number_of_edges() #8488316
+
+# print "degree_centrality: ", nx.degree_centrality(dblp)
+# print "closeness_centrality: ", nx.closeness_centrality(dblp)
+# print "betweenness_centrality: ", nx.closeness_centrality(dblp)
+
+# import matplotlib.pyplot as plt
+
+# nx.draw(dblp)
+# nx.draw_networkx(dblp, with_labels=True)
+# plt.show()
