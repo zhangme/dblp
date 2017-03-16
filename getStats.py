505d2c7e61630f11badd849ce503b80bac56b300
Meng Zhang <mengzhang@0587399095.wireless.umich.net>
2017-03-16 18:23:38 -0400

    update

--- getStats.py
+++ getStats.py
@@ -5,21 +5,18 @@ dblp = nx.read_pajek("dblp.net")
 #print "nodes: ", dblp.number_of_nodes() #1808650
 #print "edges: ", dblp.number_of_edges() #8509033
 #print "density: ", nx.density(dblp) #5.20237169758e-06
+#print "average degree: ",sum(dblp.degree().values())/float(len(dblp)) #9.40926436845
 
-# #number of components
-# print nx.number_connected_components(dblp)
-# #largest component
-# lc = sorted(nx.connected_component_subgraphs(dblp), key = len, reverse=True)[0]
-# #largest component % to whole graph
-# print float(nx.number_of_nodes(lc))/nx.number_of_nodes(dblp)
-# #largest component diameter
-# print nx.diameter(lc)
-# #largest componenet average shortest path
-# print nx.average_shortest_path_length(lc)
-# #average degree
-# print sum(dblp.degree().values())/float(len(dblp))
-# #average clustering coefficient
-# print nx.average_clustering(dblp)
+#print "number of components: ", nx.number_connected_components(dblp) #54502
+#largest component
+#lc = sorted(nx.connected_component_subgraphs(dblp), key = len, reverse=True)[0]
+#largest component % to whole graph
+#print float(nx.number_of_nodes(lc))/nx.number_of_nodes(dblp)
+#largest component diameter
+#print nx.diameter(lc)
+#largest componenet average shortest path
+#print nx.average_shortest_path_length(lc)
+
 
 
 
