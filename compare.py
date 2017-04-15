95815fd5bb7ebbf9703106f4d6f404f15d40f3b6
Meng Zhang <mengzhang@Mengs-MacBook-Pro.local>
2017-04-15 18:26:34 -0400

    nobodycare

--- compare.py
+++ compare.py
@@ -17,21 +17,22 @@ def get_jaccard_coefficient(graph,node1,node2):
 def get_shortest_path(graph,node1,node2):
     return nx.shortest_path_length(graph,node1,node2)
 
-
-name = "Li Li"
-graph = nx.read_pajek(name+".net")
-graph = multigraph2graph(graph)
-
-nodes = graph.nodes()
-for i in range(0,len(graph)-1):
-    for j in range(i+1,len(graph)):
-        name1 = nodes[i]
-        name2 = nodes[j]
-        if (name1 == name or name1[:-5].strip() == name) and (name2 == name or name2[:-5].strip() == name):
-            print name1,name2
-            if nx.has_path(graph,name1,name2):
-                print "Jaccard coef: ",get_jaccard_coefficient(graph,name1,name2)
-                print "Shortest path: ",get_shortest_path(graph,name1,name2)
-            else:
-                print "not connected"
-            print "---------------------------"
+name_list = ["Li Li"]
+for name in name_list:
+    graph = nx.read_pajek(name+".net")
+    graph = multigraph2graph(graph)
+    nodes = graph.nodes()
+    f = open(name+".csv", 'w')
+    f.write('Name1,Name2,GroundTruth,Method1,Method2,Connected\n')
+    for i in range(0,len(graph)-1):
+        for j in range(i+1,len(graph)):
+            name1 = nodes[i]
+            name2 = nodes[j]
+            if (name1 == name or name1[:-5].strip() == name) and (name2 == name or name2[:-5].strip() == name):
+                line = name1+","+name2+",F,"
+                if nx.has_path(graph,name1,name2):
+                    line = line + str(get_jaccard_coefficient(graph,name1,name2)) + "," + str(get_shortest_path(graph,name1,name2))+",F"
+                else:
+                    line = line+",,F"
+                f.write(line+"\n")
+    f.close()
